dd1b42e0a14b976b24e518ad1d00687c0c6c2e6a
kvetaren <kvet.aren@mail.ru>
2018-11-11 02:07:56 +0100

    first commit

---
+++ action_page.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    Welcome <?php echo $_POST["uid"];?><br>
+    Your email address is <?php echo $_POST["email"];?> 
+  </body>
+</html>
